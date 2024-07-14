519781c43182aea0a31c6a7b1d91bd33618743e1
Gen <ichihashigen@gmail.com>
2024-07-15 08:56:30 +0900

    フロントエンド

--- src/app/components/Summarization.tsx
+++ src/app/components/Summarization.tsx
@@ -11,9 +11,9 @@ interface SummarizationProps {
 const Summarization: React.FC<SummarizationProps> = ({ recordedText, summarizedText, inputText, onReset }) => {
   return (
     <>
-      <h2 className="text-gradient text-xl text-bold mb-4 text-center p-2">今日どうだった？</h2>
-      <div className="flex flex-wrap gap-4">
-        <div className="flex-1 bg-white p-4 rounded-lg shadow">
+      <h2 className="text-gradient text-xl text-bold mb-4 text-center p-2">「今日どうだった？」</h2>
+      <div className="flex flex-wrap gap-4 w-full">
+        <div className="flex-1 bg-white p-4 rounded-2xl shadow w-full md:w-1/2">
           <h3 className="text-gray-400 mb-2 font-bold text-center">医師向け</h3>
           <textarea
             className="w-full h-40 border border-none rounded p-2 text-slate-900"
@@ -21,7 +21,7 @@ const Summarization: React.FC<SummarizationProps> = ({ recordedText, summarizedT
             readOnly
           />
         </div>
-        <div className="flex-1 bg-white p-4 rounded-lg shadow">
+        <div className="flex-1 bg-white p-4 rounded-2xl shadow w-full md:w-1/2">
           <h3 className="text-gray-400 mb-2 font-bold text-center">患者向け</h3>
           <textarea
             className="w-full h-40 border border-none rounded p-2 text-slate-900"
